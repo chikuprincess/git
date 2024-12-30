af52ac5e7f3acb4242cc10c04937722e3bd11a3b
PRIYAVARSHINI <124878434+chikuprincess@users.noreply.github.com>
2024-12-30 10:09:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,16 @@ const connectDB = async () => {
 
 connectDB();
 
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Recipe API! Use /api/recipes to interact with the API.');
+});
+
 // Use Routes
 app.use('/api', recipeRoutes);
 
 // Port configuration
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => { // Bind to 0.0.0.0 to ensure accessibility
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
